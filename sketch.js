6a8aa764d1ac20dea45ae56cdf03bcc92958d363
rubyrubysnell <81196130+rubyrubysnell@users.noreply.github.com>
2024-02-01 16:23:52 +0000

    spacing the grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-    createCanvas(400, 400);
+    // an 800 x 800 canvas
+    createCanvas(800, 800);
+    // background colour
+    background(220)
+
+    const cellNumber = 12
+    const cellSize = 22
+    const canvasSize = 800
+    const spacing = 25
+    const startSpacing = (canvasSize - ((cellSize + spacing) * cellNumber)) / 2
+
+    for (let row = 0; row < cellNumber; row++) {
+        for (let column = 0; column < cellNumber; column++) {
+            fill(43, 43, 43)
+            noStroke()
+            const xPosition = startSpacing + (row * (cellSize + spacing))
+            const yPosition = startSpacing + (column * (cellSize + spacing))
+            const size = cellSize 
+            square(xPosition, yPosition, size)
+        }
+    }
 }
-  
-function draw() {
-    background(220);
-}
+
